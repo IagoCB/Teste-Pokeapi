e10e1ddc805df82634d58856e47b505ca7a1564d
IagoCB <49520750+IagoCB@users.noreply.github.com>
2021-11-23 21:10:28 -0300

    Criando teste de Pokeapi

---
+++ testes_karate/src/test/java/teste_api/pokemon/PokemonTestRunner.java
@@ -0,0 +1,19 @@
+package teste_api.pokemon;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class PokemonTestRunner {
+
+    @Test
+    void testParallel() {
+        Results results = Runner.path("classpath:teste_api")
+                .tags("~@ignore")
+                //.outputCucumberJson(true)
+                .parallel(5);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    }
+
+}
